5bfa2802d63d219a6825408570c2e23f2cb6a9fb
Juan Marino <juan.marino@fedex.com>
2023-04-27 09:17:59 -0500

    added graphql and lazy loading for some components

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,18 +1,21 @@
-import { CartItemContainer, ItemDetails } from './cart-item.styles';
+import { CartItemContainer, ItemDetails } from "./cart-item.styles";
+import { memo } from "react";
 
-const CartItem = ({ cartItem }) => {
-  const { name, imageUrl, price, quantity } = cartItem;
-  return (
-    <CartItemContainer>
-      <img src={imageUrl} alt={`${name}`} />
-      <ItemDetails>
-        <span>{name}</span>
-        <span>
-          {quantity} x ${price}
-        </span>
-      </ItemDetails>
-    </CartItemContainer>
-  );
-};
+//memo avoid re rendering the component if an item was already included in the cart
+
+const CartItem = memo(({ cartItem }) => {
+	const { name, imageUrl, price, quantity } = cartItem;
+	return (
+		<CartItemContainer>
+			<img src={imageUrl} alt={`${name}`} />
+			<ItemDetails>
+				<span>{name}</span>
+				<span>
+					{quantity} x ${price}
+				</span>
+			</ItemDetails>
+		</CartItemContainer>
+	);
+});
 
 export default CartItem;
