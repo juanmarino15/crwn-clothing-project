4be951bb224890e6918bd8c3fcbd93ea0398de0a
juanmarino15 <juasmarino@gmail.com>
2023-04-28 08:28:48 -0500

    added mobile view

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
 import Shop from "./routes/shop/shop.component";
 import Checkout from "./routes/checkout/checkout.component";
-import { useEffect } from "react";
+import { Fragment, Suspense, useEffect } from "react";
 import {
 	getCurrentUser,
 	onAuthStateChangedListener,
@@ -12,6 +12,8 @@ import {
 import { createUserDocumentFromAuth } from "./utils/firebase/firebase.utils";
 import { checkUserSession, setCurrentUser } from "./store/user/user.action";
 import { useDispatch } from "react-redux";
+import { GlobalStyle } from "./global.styles";
+import Spinner from "./components/spinner/spinner.component";
 
 const App = () => {
 	const dispatch = useDispatch(); //to dispatch the action to all components needing it
@@ -30,15 +32,18 @@ const App = () => {
 	}, []); //empty so only runs once(when mounting)
 
 	return (
-		<Routes>
-			<Route path="/" element={<Navigation />}>
-				<Route index element={<Home />} />
-				{/* matching all routes to shop. whatever is after / it wont matter, will get routed to shop component, then re routed to where it needs to go */}
-				<Route path="shop/*" element={<Shop />} />
-				<Route path="auth" element={<Authentication />} />
-				<Route path="checkout" element={<Checkout />} />
-			</Route>
-		</Routes>
+		<Suspense fallback={<Spinner />}>
+			<GlobalStyle />
+			<Routes>
+				<Route path="/" element={<Navigation />}>
+					<Route index element={<Home />} />
+					{/* matching all routes to shop. whatever is after / it wont matter, will get routed to shop component, then re routed to where it needs to go */}
+					<Route path="shop/*" element={<Shop />} />
+					<Route path="auth" element={<Authentication />} />
+					<Route path="checkout" element={<Checkout />} />
+				</Route>
+			</Routes>
+		</Suspense>
 	);
 };
 
